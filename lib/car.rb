c017d38cc0d1867e8594a0bd675d0d6c588944eb
Derek Haller <dhaller.personal@gmail.com>
2018-02-04 07:05:22 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,8 +1,6 @@
 require_relative "./vehicle.rb"
 class Car < Vehicle
-  class Car < Vehicle
-    def go
-      "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
-    end
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end
 end
